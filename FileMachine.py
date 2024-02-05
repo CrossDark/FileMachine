74ee37fbb7bfc9b82f6172ccf751679481e8f797
crossdark <liuhanbo333@icloud.com>
2024-02-05 18:50:53 +0800

    添加了注释功能

--- FileMachine.py
+++ FileMachine.py
@@ -2,13 +2,30 @@
 文件处理
 """
 import os
+import re
 
 
-def read_dirs(file_path):
-    """从给定的文件中读取目录列表，并返回一个目录名列表。"""
+def read_settings(file_path):
+    """
+    从给定的文件中读取目录列表，并返回一个目录名列表。
+    以以下顺序移除注释:
+    1.移除所有被<>扩起来的内容以及<>本身
+    2.移除所有#后一直到行末的字符
+    3.移除所有以#开头的行
+    :param file_path:
+    :return:
+    """
     with open(file_path, 'r') as file:
-        lines = file.read().split('\n')
-        return [line.strip().split(' = ', 1) for line in lines if line.strip()]
+        return [
+            line.strip().split(' = ', 1)
+            for line in re.sub(r'#.*\n', '\n', re.sub('<[^>]*>', '', file.read())).split('\n')
+            if line.strip() and not line.strip().startswith('#')
+        ]
+
+
+def make_settings(file_path):
+    with open(file_path, 'w') as file:
+        file.write('mode = clean')
 
 
 def cleanup_symlinks():
@@ -18,30 +35,30 @@ def cleanup_symlinks():
             os.unlink(item)
 
 
-def storage(settings_list):
+def filing(settings_list):
     """
     处理方法:切换不同文件夹组到目录下
     设置方法:mode = filing
-            path = 你选中的那组文件夹的父文件夹
+           <你选中的那组文件夹的父文件夹>(如果设置了多个父文件夹,请确保它们没有名称相同的子文件夹)
     :param settings_list:
     :return:
     """
     for sets in settings_list:
         if not sets:
             continue
-        if sets[0] == 'path':
-            for item_name in os.listdir(os.path.abspath(sets[1])):
+        if os.path.isdir(sets[0]):
+            for item_name in os.listdir(os.path.abspath(sets[0])):
                 # 跳过以'.'开头的隐藏文件和目录
                 if item_name.startswith('.'):
                     continue
                 # 创建符号链接
-                os.symlink(os.path.join(sets[1], item_name), item_name)
+                os.symlink(os.path.join(sets[0], item_name), item_name)
             print('链接成功')
         else:
             print('请设置path语句')
 
 
-def projects(settings_list):
+def working(settings_list):
     """
     处理方法:将位于不同位置的文件夹集中到当前目录下
     设置方法:mode = working
@@ -67,8 +84,12 @@ def projects(settings_list):
             print('该路径不是目录')
 
 
-def lists():
+def mix(settings_list):
     pass
+
+
+def lists():
+    read_settings()
 
 
 def exec_(get_dir):
@@ -77,23 +98,30 @@ def exec_(get_dir):
     :param get_dir:
     :return:
     """
+    print(get_dir)
     cleanup_symlinks()
     if get_dir[0][0] == 'mode':
         mode = get_dir[0][1]
+        settings = get_dir[1:]
         if mode == 'filing':
-            storage(get_dir[1:])
+            filing(settings)
         elif mode == 'working':
-            projects(get_dir[1:])
+            working(settings)
         elif mode == 'clean':
             pass
+        elif mode == 'mix':
+            mix(settings)
+
     else:
         print('请设置一种模式')
 
 
 def main():
-    exec_(read_dirs('settings.txt'))
+    try:
+        exec_(read_settings('settings.txt'))
+    except FileNotFoundError:
+        make_settings('settings.txt')
 
 
 if __name__ == '__main__':
     main()
-    
