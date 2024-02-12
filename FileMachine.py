13de2db52d1fda0a97317d9d4c294c986968bcd8
crossdark <liuhanbo333@icloud.com>
2024-02-12 16:10:37 +0800

    配置文件改为yaml成功

--- FileMachine.py
+++ FileMachine.py
@@ -3,25 +3,7 @@
 """
 import os
 import re
-import ruamel
-
-
-def read_settings(file_path):
-    """
-    从给定的文件中读取目录列表，并返回一个目录名列表。
-    以以下顺序移除注释:
-    1.移除所有被<>扩起来的内容以及<>本身
-    2.移除所有#后一直到行末的字符
-    3.移除所有以#开头的行
-    :param file_path:
-    :return:
-    """
-    with open(file_path, 'r') as file:
-        return [
-            line.strip().split(' = ', 1)
-            for line in re.sub(r'#.*\n', '\n', re.sub('<[^>]*>', '', file.read())).split('\n')
-            if line.strip() and not line.strip().startswith('#')
-        ]
+import ruamel.yaml
 
 
 def read_yaml(path):
@@ -30,15 +12,14 @@ def read_yaml(path):
     :return:
     """
     with open(path, encoding='utf-8') as file:
-        try:
-            return ruamel.yaml.YAML(typ='safe').load(re.sub('<[^>]*>', '', file.read()))
-        except ruamel.yaml.scanner.ScannerError:
-            print('YAML有问题')
+
+        return ruamel.yaml.YAML(typ='safe').load(re.sub('<[^>]*>', '', file.read()))
 
 
 def make_settings(file_path):
+    """创建设置文件"""
     with open(file_path, 'w') as file:
-        file.write('mode = clean')
+        file.write('clean')
 
 
 def cleanup_symlinks():
@@ -48,54 +29,74 @@ def cleanup_symlinks():
             os.unlink(item)
 
 
-def filing(settings_list):
-    """
-    处理方法:切换不同文件夹组到目录下
-    设置方法:mode = filing
-           <你选中的那组文件夹的父文件夹>(如果设置了多个父文件夹,请确保它们没有名称相同的子文件夹)
-    :param settings_list:
-    :return:
-    """
-    for sets in settings_list:
-        if not sets:
-            continue
-        if os.path.isdir(sets[0]):
-            for item_name in os.listdir(os.path.abspath(sets[0])):
-                # 跳过以'.'开头的隐藏文件和目录
-                if item_name.startswith('.'):
-                    continue
-                # 创建符号链接
-                os.symlink(os.path.join(sets[0], item_name), item_name)
-            print('链接成功')
+class Filing:
+    def __init__(self, settings):
+        """
+        处理方法:切换不同文件夹组到目录下
+        设置方法:mode = filing
+               <你选中的那组文件夹的父文件夹>(如果设置了多个父文件夹,请确保它们没有名称相同的子文件夹)
+        :param settings:
+        :return:
+        """
+        if type(settings) == str:
+            self.settings = settings.split(' ')
+        elif type(settings) == list:
+            self.settings = settings
         else:
-            print('请设置path语句')
+            print('不支持dict')
+            return
+        self.list()
+
+    def list(self):
+        for sets in self.settings:
+            if not sets:
+                continue
+            if os.path.isdir(sets):
+                for item_name in os.listdir(os.path.abspath(sets)):
+                    # 跳过以'.'开头的隐藏文件和目录
+                    if item_name.startswith('.'):
+                        continue
+                    # 创建符号链接
+                    os.symlink(os.path.join(sets, item_name), item_name)
+                print('链接成功')
+            else:
+                print('请设置path语句')
 
 
-def working(settings_list):
-    """
-    处理方法:将位于不同位置的文件夹集中到当前目录下
-    设置方法:mode = working
-            <项目目录1> = <项目文件夹1> <项目文件夹2>
-            …………
-    :param settings_list:
-    :return:
-    """
-    for sets in settings_list:
-        if not sets:
-            continue
-        for v in sets[1].split(' '):
-            if os.path.exists(os.path.join(sets[0], v)):
+class Working:
+    def __init__(self, settings):
+        """
+        处理方法:将位于不同位置的文件夹集中到当前目录下
+        设置方法:mode = working
+                <项目目录1> = <项目文件夹1> <项目文件夹2>
+                …………
+        :param settings:
+        :return:
+        """
+        for k, v in settings.items():
+            self.keys = k
+            self.values = v
+            if type(v) == list:
+                self.list()
+            elif type(v) == str:
+                self.values = v.split(' ')
+                self.list()
+            elif type(v) == dict:
+                self.dict()
+
+    def list(self):
+        for i in self.values:
+            if os.path.exists(os.path.join(self.keys, i)):
                 try:
-                    os.symlink(os.path.join(sets[0], v), v)
-                    print('"', v, '"', '执行成功')
+                    os.symlink(os.path.join(self.keys, i), i)
+                    print('"', i, '"', '执行成功')
                 except FileExistsError:
-                    print('没有那个文件')
+                    print('没有那个文件或文件已经存在')
             else:
                 print('Wrong')
 
-
-def mix(settings_list):
-    pass
+    def dict(self):
+        pass
 
 
 def help_():
@@ -107,49 +108,17 @@ def info():
 
 
 def lists():
-    read_settings('list.txt')
-
-
-def exec_(get_dir):
-    """
-    依据mode执行不同函数
-    :param get_dir:
-    :return:
-    """
-    print(get_dir)
-    cleanup_symlinks()
-    if get_dir[0][0] == 'mode':
-        mode = get_dir[0][1]
-        settings = get_dir[1:]
-        if mode == 'filing':
-            filing(settings)
-        elif mode == 'working':
-            working(settings)
-        elif mode == 'clean':
-            pass
-        elif mode == 'mix':
-            mix(settings)
-        elif mode == 'help':
-            help_()
-        elif mode == 'author' or 'crossdark' or 'info':
-            pass
-        else:
-            pass
-
-    else:
-        print('请设置一种模式')
+    read_yaml('list.yaml')
 
 
 def exec_dict(get: dict):
     for k, v in get.items():
         if k == 'filing':
-            filing(v)
+            Filing(v)
         elif k == 'working':
-            working(v)
+            Working(v)
         elif k == 'clean':
             pass
-        elif k == 'mix':
-            mix(v)
         elif k == 'help':
             help_()
         elif k == 'author' or 'crossdark' or 'info':
@@ -160,10 +129,14 @@ def exec_dict(get: dict):
 
 def exec_list(get: list):
     for i in get:
-        pass
+        if i == 'clean':
+            pass
+        elif i == 'help':
+            help_()
 
 
 def exec_yaml(get):
+    cleanup_symlinks()
     if type(get) == dict:
         exec_dict(get)
     elif type(get) == list:
@@ -176,9 +149,9 @@ def exec_yaml(get):
 
 def main():
     try:
-        exec_(read_settings('settings.txt'))
+        exec_yaml(read_yaml('settings.yaml'))
     except FileNotFoundError:
-        make_settings('settings.txt')
+        make_settings('settings.yaml')
 
 
 if __name__ == '__main__':
